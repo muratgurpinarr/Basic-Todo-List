aaa414b8d175687785cfc5e6ed6983a12d5e2e03
Murat Gürpınar <muratgurpinar34@gmail.com>
2025-12-07 17:43:15 +0300

    TO-DO LİST
    Basit Todo List

---
+++ script.js
@@ -0,0 +1,27 @@
+let addTodoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer')
+let inputText = document.getElementById('inputText')
+let clearTodo = document.getElementById('clearTodo')
+
+addTodoButton.addEventListener('click',function(){
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    toDoContainer.appendChild(paragraph);
+    paragraph.innerHTML = inputText.value;
+    inputText.value = "";
+
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = 'line-through'
+        
+    });
+
+    paragraph.addEventListener('dblclick',function(){
+        toDoContainer.removeChild(paragraph)
+    });
+
+    clearTodo.addEventListener('click',function(){
+        paragraph.remove();
+
+    })
+
+})
